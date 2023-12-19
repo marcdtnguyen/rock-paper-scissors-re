56e2e0f7e57ac9dfa9cbfa8b492c4c9276cb6d41
marcdtnguyen <86821559+marcdtnguyen@users.noreply.github.com>
2023-12-20 09:30:34 +1100

    Plan pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    //return 'Rock' || 'Paper' || 'Scissors'
+}
+
+function playRound(playerSelection, computerSelection){
+    //case insensitive
+    //if tie call playRound()
+    //return 'Winner' || 'Loser'
+}
+
+function game(){
+    //best of 5
+    //prompt() player input
+}
